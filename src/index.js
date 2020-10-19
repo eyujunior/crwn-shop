2a581a8ea52259141d419d3a849856a4bc712adc
eyujunior <eyujunior052@gmail.com>
2020-10-19 10:40:14 -0700

    created project with creact-react-app and add files for homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -15,3 +14,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
